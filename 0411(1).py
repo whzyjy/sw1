e8fc4b2714662752e540da99c8c563116b73bfeb
whzyjy <129816028+whzyjy@users.noreply.github.com>
2023-04-11 13:22:07 +0900

    Add files via upload

---
+++ 0411(1).py
@@ -0,0 +1,14 @@
+score=int(input("점수 입력:"))
+
+if score>=80:
+    print("합격")
+
+print("수고하셨습니다")
+
+jumsu=int(input("점수 입력:"))
+if jumsu>=80:
+    print("합격")
+else:
+     print("불합격")
+print("수고하셨습니다")
+     
---
+++ 0411(3).py
@@ -0,0 +1,11 @@
+a=float(input("a 입력:"))
+b=float(input("b 입력:"))
+c=input("c 입력:")
+if c=="+":
+    print(a+b)
+elif c=="+":
+    print(a-b)
+elif c=="+":
+    print(a*b)
+else:
+    print(a/b)
