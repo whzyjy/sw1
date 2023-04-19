a7b262adabc4d327be2b50f11ee80a38c6bc2092
whzyjy <129816028+whzyjy@users.noreply.github.com>
2023-04-19 15:20:50 +0900

    Add files via upload

---
+++ 0419/2.py
@@ -0,0 +1,19 @@
+sex=input("'M'or'm'또는'F'or'f'")
+height=float(input())
+weight=float(input())
+if(sex=='M' or sex=='m'):
+    avg=height*height*22
+    if 110<=avg<=119:
+       print("")
+    elif avg >=120:
+        print("")
+    else:
+        print("")
+else (sex=='F' or sex=='f'):
+    avg=height*height*21
+    if 110<=avg<=119:
+       print("")
+    elif avg >=120:
+        print("")
+    else:
+        print("")
